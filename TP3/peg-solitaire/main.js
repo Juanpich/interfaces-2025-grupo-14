a2954820a0f3ead074e97e07d0d35a07fec7710b
juanpi chiclana <juanpachiclanaurraco@gmail.com>
2025-10-18 17:26:01 -0300

    Reducion de archivos inecesarios

--- TP3/peg-solitaire/main.js
+++ TP3/peg-solitaire/main.js
@@ -19,3 +19,12 @@ btn_iniciar_juego.addEventListener("animationend", (e) => {
         btn_iniciar_juego.classList.add("deselected");
     }
 });
+  //Cargar FatFooter 
+  fetch('../html/fat-footer.html')
+  .then(res => res.text())
+  .then(html => {
+    document.querySelector('#fat-footer').innerHTML = html;
+    const script = document.createElement('script');
+    script.src = '../js/script.js';
+    document.body.appendChild(script);
+  });
