2e660b4a32ce546a6db046191f87ee65ee26d65f
juanpi chiclana <juanpachiclanaurraco@gmail.com>
2025-12-02 19:26:56 -0300

    Defenza

--- TP5/flappy-bird/js/Bird.js
+++ TP5/flappy-bird/js/Bird.js
@@ -2,18 +2,25 @@ class Bird {
   constructor(birdElementId, containerHeight) {
     this.bird = document.getElementById(birdElementId);
     this.containerHeight = containerHeight;
-    this.gravity = 0.8;
-    this.lift = -7;
+    this.gravity = 0.2;//baja pra la gravedad
+    this.lift = -7;//movimiento hacia arriba
     this.velocity = 0;
     this.position = this.containerHeight / 2;
-    this.maxFallSpeed = 4;
+    this.maxFallSpeed = 2;//velocidad de caida
     this.isAlive = true;
     this.gameStarted = false;
     this.isBlinking = false; // para el parpadeo
     this.isFrozen = false;
     this.radius = this.bird.offsetWidth / 2; // para colisión circular
     this.sonidoVuelo = new Audio("./../audio/vuelo.mp3");
+    this.contadorMovimiento = 0;
+    this.contadorMovimientoCada = 0
     this.init();
+     setInterval(() => {
+      console.log("sumo tiempo")
+      this.contadorMovimientoCada += 1000
+    }, 1000)
+
   }
 
   init() {
@@ -25,6 +32,8 @@ class Bird {
         this.sonidoVuelo.play();
         if (!this.gameStarted) {
           this.gameStarted = true;
+          this.contadorMovimiento = 7000
+          this.contadorMovimientoCada = 0
           this.update();
           document.dispatchEvent(new Event('game-started'));
         }
@@ -37,6 +46,8 @@ class Bird {
     if (!this.isAlive) return;
     if (this.isFrozen) return;
     this.velocity = this.lift;
+    
+
   }
 
 
@@ -57,76 +68,90 @@ class Bird {
       this.velocity = 0;
       this.hitPipe();
     }
+    if (this.contadorMovimiento === this.contadorMovimientoCada) {
+      console.log("Cambio la velocidad ")
+      this.contadorMovimientoCada = 0;
+      if (this.gravity < 0.6) {
+        this.gravity += 0.1
+      }
+      if (this.maxFallSpeed < 4) {
+        this.maxFallSpeed += 0.5
+      }
+    }
+   
+
+
 
     this.bird.style.top = `${this.position}px`;
     requestAnimationFrame(() => this.update());
   }
-
- hitPipe() {
-  // Evitar perder vida múltiple
-  if (this.isBlinking) return;
-
-  this.isBlinking = true;
-  document.dispatchEvent(new Event("hit-limit"));
-
-  // Crear y mostrar explosión
-  this.showExplosion();
-
-  // Congelar por un momento
-  this.isFrozen = true;
-  const originalGravity = this.gravity;
-  this.gravity = 0;
-  this.velocity = 0;
-
-  // A media pantalla
-  this.position = this.containerHeight / 2;
-  this.bird.style.top = `${this.position}px`;
-
-  const freezeTime = 150;
-
-  // Restore gravity y permitir flap normalmente
-  setTimeout(() => {
-    this.gravity = originalGravity;
-    this.isFrozen = false;
-  }, freezeTime);
-
-  // Parpadeo visual del pájaro
-  let visible = true;
-  const blinkInterval = setInterval(() => {
-    this.bird.style.opacity = visible ? "0.3" : "1";
-    visible = !visible;
-  }, 150);
-
-  setTimeout(() => {
-    clearInterval(blinkInterval);
-    this.bird.style.opacity = "1";
-    this.isBlinking = false;
-  }, 1500);
-}
-
-showExplosion() {
-  this.bird.style.opacity = '0'; // Ocultar el pájaro temporalmente
-  // Crear elemento de explosión
-  const explosion = document.createElement('div');
-  explosion.className = 'explosion';
   
-  // Posicionar en el lugar del pájaro
-  const birdRect = this.bird.getBoundingClientRect();
-  const containerRect = this.bird.parentElement.getBoundingClientRect();
-  
-  explosion.style.position = 'absolute';
-  explosion.style.left = `${birdRect.left - containerRect.left}px`;
-  explosion.style.top = `${birdRect.top - containerRect.top}px`;
-  explosion.style.zIndex = '100';
-  
-  // Agregar al contenedor
-  this.bird.parentElement.appendChild(explosion);
-  
-  // Remover después de que termine la animación (500ms)
-  setTimeout(() => {
-    explosion.remove();
-  }, 500);
-}
+
+  hitPipe() {
+    // Evitar perder vida múltiple
+    if (this.isBlinking) return;
+
+    this.isBlinking = true;
+    document.dispatchEvent(new Event("hit-limit"));
+
+    // Crear y mostrar explosión
+    this.showExplosion();
+
+    // Congelar por un momento
+    this.isFrozen = true;
+    const originalGravity = this.gravity;
+    this.gravity = 0;
+    this.velocity = 0;
+
+    // A media pantalla
+    this.position = this.containerHeight / 2;
+    this.bird.style.top = `${this.position}px`;
+
+    const freezeTime = 150;
+
+    // Restore gravity y permitir flap normalmente
+    setTimeout(() => {
+      this.gravity = originalGravity;
+      this.isFrozen = false;
+    }, freezeTime);
+
+    // Parpadeo visual del pájaro
+    let visible = true;
+    const blinkInterval = setInterval(() => {
+      this.bird.style.opacity = visible ? "0.3" : "1";
+      visible = !visible;
+    }, 150);
+
+    setTimeout(() => {
+      clearInterval(blinkInterval);
+      this.bird.style.opacity = "1";
+      this.isBlinking = false;
+    }, 1500);
+  }
+
+  showExplosion() {
+    this.bird.style.opacity = '0'; // Ocultar el pájaro temporalmente
+    // Crear elemento de explosión
+    const explosion = document.createElement('div');
+    explosion.className = 'explosion';
+
+    // Posicionar en el lugar del pájaro
+    const birdRect = this.bird.getBoundingClientRect();
+    const containerRect = this.bird.parentElement.getBoundingClientRect();
+
+    explosion.style.position = 'absolute';
+    explosion.style.left = `${birdRect.left - containerRect.left}px`;
+    explosion.style.top = `${birdRect.top - containerRect.top}px`;
+    explosion.style.zIndex = '100';
+
+    // Agregar al contenedor
+    this.bird.parentElement.appendChild(explosion);
+
+    // Remover después de que termine la animación (500ms)
+    setTimeout(() => {
+      explosion.remove();
+    }, 500);
+  }
 
 
   respawn() {
